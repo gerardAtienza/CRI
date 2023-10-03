af8258c68f8636d7e5b965b41421a006d33163c6
Gerard <gerix.ati@gmail.com>
2023-10-03 10:54:50 +0200

    kmiok

---
+++ main.py
@@ -0,0 +1,33 @@
+#Implementació dels mots encreuats utilitzant backtracking
+
+#Llegim fitxers
+def llegeixFitxers(fitxer1, fitxer2):
+    dicc = open(fitxer1)
+    paraules = dicc.readlines()
+
+    dicc = open(fitxer2)
+    tauler = ''.join(dicc.readlines()).replace('\n',';')
+
+    return paraules, tauler
+
+def ordenaParaules(paraules):
+    pars = {}
+    for paraula in paraules:
+        p = paraula.strip() #Treu el \n.
+        l = len(p)
+
+        if l in pars.keys():
+            pars[l].append(p)
+        else:
+            pars[l] = [p]
+
+    pars = sorted(pars.items())
+    return pars
+
+def aplicaRestriccions(paraules, tauler):
+    col = 
+    
+
+paraules, tauler = llegeixFitxers('diccionari_CB_v3.txt', 'crossword_CB_v3.txt')
+words = ordenaParaules(paraules)
+print(words)
